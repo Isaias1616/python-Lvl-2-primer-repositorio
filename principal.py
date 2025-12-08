be4bffcf38dea069eec9cc3b716e78dc40300971
Isaias1616 <isaiasperezbenjamin@gmail.com>
2025-12-08 19:15:53 -0400

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,1 +1,16 @@
-
+while True:
+    meme_dict = {
+                "CRINGE": "Algo excepcionalmente raro o embarazoso",
+                "LOL": "Una respuesta común a algo gracioso",
+                "ROFL": "una respuesta a una broma",
+                "SHEESH": "ligera desaprobación",
+                "CREEPY": "aterrador, siniestro",
+                "AGGRO": "ponerse agresivo/enojado"
+                }
+    
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("Esa palabra no se encuentra en el diccionario!!! Intenta nuevamente...")
